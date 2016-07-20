d77f70a79b83cbfc2d11038f5176ba269934eb8a
noby <n.fujioka@abacusmaths.info>
2016-07-20 17:12:42 +0100

    ex22 attack

--- app.rb
+++ app.rb
@@ -19,6 +19,12 @@ class Battle < Sinatra::Base
     erb(:play)
   end
 
+  get '/attack' do
+    @name1 = session["Name1"]
+    @name2 = session["Name2"]
+    erb :attack  
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -2,6 +2,6 @@ feature "Attack Player" do
   scenario "player attacks another" do
     sign_in_and_play
     click_button "attack"
-    expect(page).to have_content('Attacked Noby')
+    expect(page).to have_content('Albie attacked Noby')
   end
 end

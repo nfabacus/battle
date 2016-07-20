a6ec97ba3e17127e53a11edd93ae5047bd25005a
albiebabie <albiebabie@gmail.com>
2016-07-20 14:37:55 +0100

    challenge 18 - passing first test

--- app.rb
+++ app.rb
@@ -4,6 +4,11 @@ class Battle < Sinatra::Base
   get '/' do
     erb(:index)
   end
+  post '/names' do
+    @name1 = params[:Name1]
+    @name2 = params[:Name2]
+    erb(:play)
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/name_form_spec.rb
+++ spec/features/name_form_spec.rb
@@ -2,10 +2,10 @@ RSpec.feature "Name form", :type => :feature do
   scenario "User types name" do
     visit "/"
 
-    fill_in "Name1", :with => "Albie"
-    fill_in "Name2", :with => "Noby"
+    fill_in "Name1", with: "Albie"
+    fill_in "Name2", with: "Noby"
     click_button "submit"
-    expect(index).to have_text("Player1 is Albie. Player2 is Noby.")
+    expect(page).to have_content('Player 1 is Albie. Player 2 is Noby.')
 
   end
 end

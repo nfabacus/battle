b5289412b77f9a90fc96d03776f5e3e20df27679
noby <n.fujioka@abacusmaths.info>
2016-07-21 16:18:44 +0100

    adds one test to attack spec.

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,6 +4,7 @@ feature "Attack Player" do
     click_button "attack"
     expect(page).to have_content('Albie attacked Noby')
     click_button "ok"
+    expect(page).not_to have_content("Noby's: 50HP")
     expect(page).to have_content("Noby's: 40HP")
   end
 
